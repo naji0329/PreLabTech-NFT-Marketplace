7fdd2f7dfe984e35c350b297fc0e3ee2a2ec384c
NAJI2329 <be1512t6@gmail.com>
2022-06-23 20:17:07 +0845

    update my collection page

--- frontend/src/pages/MyCollection.vue
+++ frontend/src/pages/MyCollection.vue
@@ -11,10 +11,45 @@
 
 
   <div class="container">
-    <router-link class="dropdown-item card-generic-item" to="/create-collection">
-      <button class="btn btn-primary" type="button" >Create Collection</button>
-    </router-link>
+    <!-- <router-link class="dropdown-item card-generic-item" to="/create-collection"> -->
+      <button class="btn btn-primary" type="button"  data-bs-toggle="modal" data-bs-target="#cctModal">Create Collection</button>
+    <!-- </router-link> -->
   </div>
+
+  <!-- Modal -->
+  <div class="modal fade" id="cctModal" tabindex="-1" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered">
+      <div class="modal-content">
+        <div class="modal-body">
+          <h2>Choose Type</h2>
+          <p>Choose “Single” for one of a kind or “Multiple” if you want to sell one collectible multiple times</p>
+          
+          <div class="row g-gs">
+              <div class="col-sm-6 col-6">
+                  <router-link to="create-collection" class="card text-center card-full" v-on:click="createSingleCollection()"  data-bs-dismiss="modal">
+                      <div class="card-body card-body-s1 d-block">
+                          <img class="mb-4 create-img"  src="@/images/thumb/single-mobile.png" alt="" />
+                          <span class="text-black fw-bold d-block">Single</span>
+                      </div>
+                  </router-link>
+              </div><!-- enbd col -->
+              <div class="col-sm-6 col-6">
+                  <router-link to="create-singleCollection" class="card text-center card-full"  v-on:click="createMultipleCollection()"  data-bs-dismiss="modal">
+                      <div class="card-body card-body-s1 d-block">
+                          <img class="mb-4 create-img"  src="@/images/thumb/multiple-mobile.png" alt="" />
+                          <span class="text-black fw-bold d-block">Multiple</span>
+                      </div>
+                  </router-link>
+              </div><!-- enbd col -->
+          </div><!-- end row -->
+        </div>
+      </div>
+      <!-- end modal-content -->
+    </div>
+    <!-- end modal-dialog -->
+  </div>
+  <!-- end modal-->
+  
 
   <MyCollectionSection></MyCollectionSection>
   <Footer></Footer>
@@ -39,6 +74,14 @@ export default {
   },
   mounted() {
     
+  },
+  methods: {
+    createSingleCollection: function() {
+      this.$router.push({ name: 'create-single-collection' })
+    },
+    createMultipleCollection: function() {
+      this.$router.push({ name: 'create-multiple-collection' })
+    }
   },
 
 }
