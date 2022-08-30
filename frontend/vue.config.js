d9b64758df8afd546ea395abe74d1c371bdb1877
NAJI2329 <be1512t6@gmail.com>
2022-08-30 08:47:12 +0000

    Update PORT

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -2,7 +2,7 @@ module.exports = {
     publicPath: process.env.NODE_ENV === 'production' ? '' : '/',
     productionSourceMap: false,
     devServer: {
-        proxy: 'http://127.0.0.1:5000/api',
+        proxy: 'http://127.0.0.1:5003/api',
         port: 8000,
         disableHostCheck: true
     }
