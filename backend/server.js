7c4b23ee11ed5be9a3f829c6718724ec9ad4332a
NAJI2329 <be1512t6@gmail.com>
2022-09-01 09:18:27 +0000

    Change Backend PORT

--- backend/server.js
+++ backend/server.js
@@ -41,6 +41,6 @@ if (process.env.NODE_ENV === 'production') {
   });
 }
 
-const PORT = process.env.PORT || 5003;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
